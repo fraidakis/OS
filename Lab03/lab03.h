87bdd1968ef06629e65b478db5ad8727b3b2fb5b
Your Name <you@example.com>
2024-12-02 13:17:10 +0200

    Initial commit

---
+++ Lab03/lab03.h
@@ -0,0 +1,14 @@
+#ifndef LAB_H3
+#define LAB_H3
+
+// Function declaration
+void print_message(void);
+
+// Function definition
+#include <stdio.h>
+
+void print_message(void) {
+    printf("hello world i am lab03\n");
+}
+
+#endif /* LAB_H3 */
